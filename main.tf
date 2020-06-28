5a0e4fdaaaa24f1b65509fefcb50dbaeb188471a
unknown <Robert.Aron.tx@gmail.com>
2020-06-27 22:17:58 -0500

    it's wrking!

--- main.tf
+++ main.tf
@@ -5,10 +5,18 @@ provider "google" {
 
   project = "lightbikenode"
   region  = "us-central1"
-  zone    = "us-central1-c"
+  zone    = "us-central1-a"
+}
+provider "google-beta" {
+
+  credentials = file("./creds/LightBikeNode-1548ac937c58.json")
+
+  project = "lightbikenode"
+  region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
-resource "google_compute_instance" "instance" {
+resource "google_compute_instance" "light_bike_server" {
   boot_disk {
     initialize_params {
       image = "cos-cloud/cos-stable-72-11316-171-0"
@@ -46,13 +54,111 @@ resource "google_compute_instance" "instance" {
   }
 }
 
-resource "google_compute_firewall" "default" {
- name    = "allow-web-traffic"
- network = "default"
+# Probably remove this
 
- allow {
-   protocol = "tcp"
-   ports    = ["80","443"]
- }
+resource "google_compute_firewall" "default" {
+  name    = "allow-web-traffic"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
 }
 
+# LB Stuff
+
+resource "google_compute_instance_group" "instance_group" {
+  name      = "light-bike-instance-group"
+  instances = [google_compute_instance.light_bike_server.self_link]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  named_port {
+    name = "http"
+    port = 80
+  }
+}
+
+resource "google_compute_http_health_check" "default" {
+  name               = "http-health-check"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
+
+resource "google_compute_backend_service" "default" {
+  name        = "light-bike-backend-service"
+  port_name   = "http"
+  protocol    = "HTTP"
+
+  backend {
+    group = google_compute_instance_group.instance_group.self_link
+  }
+
+  health_checks = [google_compute_http_health_check.default.self_link]
+}
+
+
+
+
+// This is the actual load balancer
+resource "google_compute_url_map" "default" {
+  name        = "url-map"
+  default_service = google_compute_backend_service.default.id
+}
+
+resource "google_compute_global_address" "default" {
+  name         = "test-global-address"
+  ip_version   = "IPV4"
+  address_type = "EXTERNAL"
+}
+####################################################
+#################################################### 
+# HTTPS
+####################################################
+####################################################
+resource "google_compute_managed_ssl_certificate" "default" {
+  provider = google-beta
+  name = "test-cert"
+  managed {
+    domains = ["light.bike.robertaron.io"]
+  }
+}
+
+resource "google_compute_target_https_proxy" "target_https" {
+  name             = "https-target-proxy"
+  url_map          = google_compute_url_map.default.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
+}
+
+
+// Frontend config
+resource "google_compute_global_forwarding_rule" "forward_rule_https" {
+  name       = "forward-https-rule"
+  target     = google_compute_target_https_proxy.target_https.self_link // connection to the connection to the load balancer
+  ip_address = google_compute_global_address.default.address // input ip adress
+  port_range = "443"
+}
+
+####################################################
+#################################################### 
+# HTTP
+####################################################
+####################################################
+
+resource "google_compute_target_http_proxy" "target_http" {
+  name             = "https-target-proxy"
+  url_map          = google_compute_url_map.default.id
+}
+
+
+// Frontend config
+resource "google_compute_global_forwarding_rule" "forward_rule_http" {
+  name       = "forward-http-rule"
+  target     = google_compute_target_http_proxy.target_http.self_link // connection to the connection to the load balancer
+  ip_address = google_compute_global_address.default.address // input ip adress
+  port_range = "80"
+}
